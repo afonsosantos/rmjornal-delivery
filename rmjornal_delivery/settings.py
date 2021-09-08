f5095d13d5657c876ac91e67ce9d4237dc4572a4
Afonso Santos <afonso.faisca1@gmail.com>
2021-09-08 12:05:53 +0100

    Fix static files

--- rmjornal_delivery/settings.py
+++ rmjornal_delivery/settings.py
@@ -121,10 +121,4 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Static files (CSS, JavaScript, Images)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
